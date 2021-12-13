52fd7f5c25cc78df05801bbc91140de05c134d4d
Pavel Pelikh <barashkaaaa@gmail.com>
2021-12-13 21:26:22 +0200

    Implement chain class

--- 4/composer.py
+++ 4/composer.py
@@ -1,4 +1,5 @@
 import numpy as np
+from misc import memoize
 from tqdm import trange
 
 
@@ -8,8 +9,8 @@ from tqdm import trange
 
 class Chain:
     def __init__(self,
-                 img_chains: list[np.ndarray],
-                 msk_chains: list[np.ndarray],
+                 img_chains: np.ndarray,
+                 msk_chains: np.ndarray,
                  alpha: float = 1,
                  beta: float = 1
                  ) -> None:
@@ -19,14 +20,62 @@ class Chain:
         self.img_chains = img_chains
         self.msk_chains = msk_chains
 
-    def point_weight(self, idx: int, k: int) -> float:
-        raise NotImplementedError
+        self.__weights = self.alpha * (1 - self.msk_chains)
+        self.__class_num = img_chains.shape[0]
+        self.__pts_num = self.img_chains.shape[1]
 
-    def edge_weight(self, idx1: int, k1: int, idx2: int, k2: int) -> float:
-        raise NotImplementedError
+    def point_weight(self,
+                     idx: int,
+                     k: int) -> float:
+        if idx >= self.__pts_num or k >= self.__class_num or idx == -1 or k == -1:
+            return np.inf
+
+        return self.__weights[k, idx]
+
+    @memoize
+    def edge_weight(self,
+                    idx1: int,
+                    idx2: int,
+                    k1: int,
+                    k2: int) -> float:
+        if abs(idx1 - idx2) != 1 and (idx1 < 0 or idx2 < 0):
+            return np.inf
+
+        return self.beta * (np.sum(np.abs(self.img_chains[k1, idx1] - self.img_chains[k2, idx1])) +
+                            np.sum(np.abs(self.img_chains[k1, idx2] - self.img_chains[k2, idx2])))
 
     def compose(self) -> np.ndarray:
-        raise NotImplementedError
+        _, labels = self.__f()
+
+        return np.array([self.img_chains[labels[i], i] for i in range(self.__pts_num)])
+
+    @memoize
+    def __f(self, idx: int = 0, k: int = -1) -> tuple[float, list[int]]:
+        if idx >= self.__pts_num or k >= self.__class_num:
+            return 0, []
+
+        if k == -1:
+            k_range = range(self.__class_num)
+        else:
+            k_range = [k]
+
+        k_min_val = np.inf
+        best_labels = []
+        for k in k_range:
+            k_next_min_val = np.inf
+            best_next_labels = []
+            for k_next in range(self.__class_num):
+                f_next, next_labels = self.__f(idx+1, k_next)
+                val = self.point_weight(idx+1, k+1) + self.edge_weight(idx, idx+1, k, k_next) + f_next
+                if val < k_next_min_val:
+                    best_next_labels = next_labels
+                    k_next_min_val = val
+
+            if k_next_min_val < k_min_val:
+                best_labels = [k] + best_next_labels
+                k_min_val = k_next_min_val
+
+        return k_min_val, best_labels
 
 
 class Composer:
