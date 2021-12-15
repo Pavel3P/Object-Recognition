b1eec8638f2bc3128cd931da0f9434bdced53da9
ArturMarkov <azoveagl@gmail.com>
2021-12-15 02:26:50 +0200

    Point and edge weights added

--- 4/composer.py
+++ 4/composer.py
@@ -19,10 +19,21 @@ class Composer:
         self.width = int(self.images.shape[2])
 
     def __get_points_weights(self) -> np.ndarray:
-        raise NotImplementedError
+        point_weights = np.zeros((self.class_num, self.height, self.width+1))
+        point_weights[:, :, :-1] = self.alpha * (1 - self.masks)
+        return point_weights
 
     def __get_edges_weights(self) -> np.ndarray:
-        raise NotImplementedError
+        edge_weights = np.zeros(
+            (self.class_num, self.class_num, self.height, self.width)
+        )
+        for k1 in range(self.class_num):
+            for k2 in range(self.class_num):
+                diff = np.linalg.norm(
+                    self.images[k1] - self.images[k2], axis=2, ord=1
+                )
+                edge_weights[k1, k2, :, :-1] = self.beta * (diff[:, :-1] + diff[:, 1:])
+        return edge_weights
 
     def __get_f(self) -> np.ndarray:
         raise NotImplementedError
