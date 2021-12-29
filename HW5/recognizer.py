e42ddf0d32fd87e33ee50b25b937274fd67f5f1d
Pavel Pelikh <barashkaaaa@gmail.com>
2021-12-29 13:18:28 +0200

    Add recognizer

--- HW5/recognizer.py
+++ HW5/recognizer.py
@@ -1,9 +1,21 @@
 import numpy as np
+from typing import Union
 
 
 class Recognizer:
-    def __init__(self, method: str = "simple", *args, **kwargs) -> None:
-        raise NotImplementedError
+    def __init__(self, method: str = "simple", **kwargs) -> None:
+        self.method = method
 
-    def __call__(self, image: np.ndarray, label: int) -> bool:
-        raise NotImplementedError
+        if self.method == "simple":
+            self.samples: dict[Union[int, str]: np.ndarray] = kwargs["samples"]
+            self.recognize: callable = self.__recognize_simple
+        else:
+            raise NotImplementedError
+
+    def __recognize_simple(self, image: np.ndarray, label: int) -> bool:
+        if label not in self.samples.keys():
+            return False
+        elif image.shape != self.samples[label].shape:
+            return False
+        else:
+            return np.all(image == self.samples[label])
